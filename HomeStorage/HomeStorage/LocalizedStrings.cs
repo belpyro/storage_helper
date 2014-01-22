f29977c8088abbf0088ba0e9e0d397b6bf982be2
Alexander Shaduro <alexandersha@playtika.by>
2014-01-22 15:56:12 +0300

    + added new sources

---
+++ HomeStorage/HomeStorage/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using HomeStorage.Resources;
+
+namespace HomeStorage
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
